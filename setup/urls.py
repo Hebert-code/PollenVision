5460cf3a973b1fbbe62f956ef3a78af2b843e457
aureliodeboa <aurelio74123@gmail.com>
2024-09-15 18:10:30 -0300

    feat: implements home page

--- setup/urls.py
+++ setup/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')), #home page
     path('admin/', admin.site.urls),
     path('pollenvision/', include('pollenvision.urls'))
 ]
