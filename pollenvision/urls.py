b26d409afcbf5856ea745f7efc0bffa608dff299
Hebert-code <hebert.hb.brito@gmail.com>
2024-09-19 22:01:58 -0300

    Urls

--- pollenvision/urls.py
+++ pollenvision/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from pollenvision.views import index
+from pollenvision.views import index, dashboard, upload, resultado, historico, relatorios, suporte
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('upload/', upload, name='upload'),
+    path('resultado/', resultado, name='resultado'),
+    path('historico/', historico, name='historico'),
+    path('relatorios/', relatorios, name='relatorios'),
+    path('suporte/', suporte, name='suporte')
 ]
--- pollenvision/views.py
+++ pollenvision/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'pollenvision/index.html')
+    return render(request, 'pollenvision/base.html')
+
+def dashboard(request):
+    return render(request, 'pollenvision/dashboard.html')
+
+def upload(request):
+    return render(request, 'pollenvision/upload.html')
+
+def resultado(request):
+    return render(request, 'pollenvision/resultado.html')
+
+def historico(request):
+    return render(request, 'pollenvision/historico.html')
+
+def relatorios(request):
+    return render(request, 'pollenvision/relatorios.html')
+
+def suporte(request):
+    return render(request, 'pollenvision/suporte.html')
