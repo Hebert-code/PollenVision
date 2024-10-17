5b46a3ae296e9764323f6cd8253ea6669e5cc22d
Alysson-Alves23 <alyssonja23@gmail.com>
2024-10-16 22:16:23 -0300

    dashboard responsive

--- static/home/js/scripts.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('menu-toggle').addEventListener('click', function() {
-        var navLinks = document.getElementById('nav-links');
-        navLinks.classList.toggle('active');
-    });
