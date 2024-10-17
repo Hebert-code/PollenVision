f2318eb98de4be31811ef0fee2af0006bd6a6135
Alysson-Alves23 <alyssonja23@gmail.com>
2024-10-16 21:50:11 -0300

    Index.html reponsive

---
+++ static/home/js/scripts.js
@@ -0,0 +1,5 @@
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+        var navLinks = document.getElementById('nav-links');
+        navLinks.classList.toggle('active');
+    });
