8dbbe3e73a2b2c2c375347bc3f7e732416b1658a
aureliodeboa <aurelio74123@gmail.com>
2024-11-27 14:08:03 -0300

    bug fix: dowload weights

--- static/yolo/yologdow.py
+++ static/yolo/yologdow.py
@@ -1,8 +1,9 @@
 import gdown
 
-# URL do link público do Google Drive
-url = "https://drive.google.com/file/d/1wx3_TFZsvfB4jKUw6CA3yt2HuGiktSKn"
-output = "yolov4_custom_3000.weights"  # Nome do arquivo local
+# ID do arquivo extraído da URL
+file_id = "1wx3_TFZsvfB4jKUw6CA3yt2HuGiktSKn"
+url = f"https://drive.google.com/uc?id={file_id}"  # URL correta para o gdown
+output = "yolov4_custom_3000.weights"
 
 # Baixar o arquivo
 gdown.download(url, output, quiet=False)
